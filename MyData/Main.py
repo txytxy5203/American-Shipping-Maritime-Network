cc37a41240a8257abbcf6665372aef307d0b43ff
学有 谈 <168973291@qq.com>
2025-11-18 10:36:03 +0800

    get_networks_by_seasons

--- MyData/Main.py
+++ MyData/Main.py
@@ -62,3 +62,27 @@ class Main:
                 if time == year_month:
                     return DiG, G
         return None, None
+
+    @classmethod
+    def get_networks_by_seasons(cls):
+        """
+        网络生成器函数   seasons
+        :return: 每次生成对应的 DiG  G  time
+        """
+        years = range(2017, 2022)
+        seasons = ['Spring', 'Summer', 'Autumn', 'Winter']
+        # 读取数据并构建网络
+        for year in years:
+            for season in seasons:
+                # 跳过2021年夏季及以后（数据不全）
+                if year == 2021 and season in ['Summer', 'Autumn', 'Winter']:
+                    continue
+                file_path = f'../Data/{year}/US/Season/{season}/US{year}_{season}_Digraph.graphml'
+                if not os.path.exists(file_path):
+                    print(f'⚠️ 文件不存在: {file_path}')
+                    continue
+                time = f"{year} {season}"
+
+                DiG = nx.read_graphml(file_path)
+                G = nx.Graph(DiG)
+                yield DiG, G, time
